d9982ba32006ff5f5a265825fda5be8a74c74305
Erik McComb <erik@erikmccomb.com>
2017-09-19 15:16:17 -0600

    working todolist mimic with blog variables. better strting point for new features.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import EntryForm from './components/EntryForm';
+import EntryList from './components/EntryList';
+import Footer from './components/Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+// app will not hold state here, state will be held in redux. app can be a
+// function
+const App = () => (
+  <div>
+    <EntryForm/>
+    <EntryList/>
+    <Footer/>
+  </div>
+)
+
+// No functions arrays or state passed down
 
 export default App;
--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {toggleTodo} from '../actions/entries';
+import {toggleEntry} from '../actions/entries';
 
 const styles = {
   complete: {
